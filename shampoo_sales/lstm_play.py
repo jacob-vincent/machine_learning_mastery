cd6acc4573620cd6b02b7973272be8888a09cfb1
Vincent <jvincen7@LAMU02TC0RAGTFM.uhc.com>
2018-02-07 14:48:32 -0600

    added model evaluation script

--- shampoo_sales/lstm_play.py
+++ shampoo_sales/lstm_play.py
@@ -1,13 +1,21 @@
-from __future__ import absolute_import
+# Silence TensorFlow warnings
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # or any {'0', '1', '2'}
+import tensorflow as tf
+
+# Import Statements
+#from __future__ import absolute_import
 from pandas import DataFrame, Series, concat, read_csv, datetime
 from sklearn.metrics import mean_squared_error
 from sklearn.preprocessing import MinMaxScaler, StandardScaler
-import numpy
+from sklearn.externals import joblib
 from keras.models import Sequential
 from keras.layers import Dense, LSTM, TimeDistributed
+from keras.callbacks import LambdaCallback, History
 from math import sqrt
 from matplotlib import pyplot
-
+import numpy
+import pickle
 
 # date-time parsing function for loading the dataset
 def parser(x):
@@ -48,13 +56,6 @@ def scale(train, test):
 	test_scaled = scaler.transform(test)
 	return scaler, train_scaled, test_scaled
 
-# inverse scaling for a forecasted value
-def invert_scale(scaler, X, value):
-	new_row = [x for x in X] + [value]
-	array = numpy.array(new_row)
-	array = array.reshape(1, len(array))
-	inverted = scaler.inverse_transform(array)
-	return inverted[0, -1]
 
 # fit an LSTM network to training data
 def fit_lstm(train, test, batch_size, nb_epoch, neurons):
@@ -64,25 +65,18 @@ def fit_lstm(train, test, batch_size, nb_epoch, neurons):
 	x_test, y_test = test[:, 0:-1], test[:, -1]
 	x_test = x_test.reshape(x_test.shape[0], 1, x_test.shape[1])
 
+	history = History()
+
 	print(X.shape)
 	print(x_test.shape)
 	model = Sequential()
 	model.add(LSTM(neurons, batch_input_shape=(batch_size, X.shape[1], X.shape[2]), stateful=True, return_sequences=True))
 	model.add(Dense(2*neurons, activation='relu'))
 	model.add(LSTM(neurons, stateful=True, return_sequences=False))
-	model.add(Dense(1))
+	model.add(Dense(1, activation='sigmoid'))
 	model.compile(loss='mean_squared_error', optimizer='adam')
-	for i in range(nb_epoch):
-		print ('Round ' + str(i) + ' of ' + str(nb_epoch))
-		model.fit(X, y, epochs=1, batch_size=batch_size, verbose=1, shuffle=False, validation_data=(x_test, y_test))
-		model.reset_states()
+	model.fit(X, y, epochs=nb_epoch, batch_size=batch_size, verbose=1, shuffle=False, validation_data=(x_test, y_test), callbacks=[history])
 	return model
-
-# make a one-step forecast
-def forecast_lstm(model, batch_size, X):
-	X = X.reshape(1, 1, len(X))
-	yhat = model.predict(X, batch_size=batch_size)
-	return yhat[0,0]
 
 # load dataset
 series = read_csv('~/Desktop/Machine_learning_mastery/shampoo_sales/shampoo_sales_data.csv', header=0, parse_dates=[0], index_col=0, squeeze=True, date_parser=parser)
@@ -95,43 +89,34 @@ diff_values = difference(raw_values, 1)
 supervised = timeseries_to_supervised(diff_values, 1)
 supervised_values = supervised.values
 
-# split data into train and test-sets
+# split data into train and test-sets, saving results
 train, test = supervised_values[0:-12], supervised_values[-12:]
+numpy.savetxt('./shampoo_sales/train_df.csv', train, delimiter=',')
+numpy.savetxt('./shampoo_sales/test_df.csv', test, delimiter=',')
 
 # transform the scale of the data
 scaler, train_scaled, test_scaled = scale(train, test)
 
+# Save scaler object and scaled data
+joblib.dump(scaler, './shampoo_sales/lstm_scaler.pkl')
+numpy.savetxt('./shampoo_sales/train_scaled_df.csv', train_scaled, delimiter=',')
+numpy.savetxt('./shampoo_sales/test_scaled_df.csv', test_scaled, delimiter=',')
+
 print(train_scaled.shape, test_scaled.shape)
 
 # fit the model
-lstm_model = fit_lstm(train_scaled, test_scaled, 1, 3000, 4)
+lstm_model = fit_lstm(train_scaled, test_scaled, 1, 20, 4)
+
 # forecast the entire training dataset to build up state for forecasting
 train_reshaped = train_scaled[:, 0].reshape(len(train_scaled), 1, 1)
 lstm_model.predict(train_reshaped, batch_size=1)
 
-# walk-forward validation on the test data
-predictions = list()
-for i in range(len(test_scaled)):
-	# make one-step forecast
-	X, y = test_scaled[i, 0:-1], test_scaled[i, -1]
-	yhat = forecast_lstm(lstm_model, 1, X)
-	# invert scaling
-	yhat = invert_scale(scaler, X, yhat)
-	# invert differencing
-	yhat = inverse_difference(raw_values, yhat, len(test_scaled)+1-i)
-	# store forecast
-	predictions.append(yhat)
-	expected = raw_values[len(train) + i + 1]
-	print('Month=%d, Predicted=%f, Expected=%f' % (i+1, yhat, expected))
-
-# report performance
-rmse = sqrt(mean_squared_error(raw_values[-12:], predictions))
-print('Test RMSE: %.3f' % rmse)
-# line plot of observed vs predicted
-pyplot.plot(raw_values[-12:])
-pyplot.plot(predictions)
-pyplot.show()
-
-# Evaluate underfitting/overfitting
-pyplot(lstm_model.history['loss'])
-pyplot(lstm_model.history[''])
+# Save model
+# serialize model to JSON
+model_json = lstm_model.to_json()
+with open("./shampoo_sales/model.json", "w") as json_file:
+    json_file.write(model_json)
+# serialize weights to HDF5
+lstm_model.save_weights("./shampoo_sales/model.h5")
+print("Saved model to disk")
+print(lstm_model.history.keys())
